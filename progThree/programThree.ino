96026fdbe433564f17245650466e7aba9dd88480
Paul Borawski <paulborawski14@gmail.com>
2016-03-17 16:48:31 -0700

    added third project

---
+++ progThree/programThree.ino
@@ -0,0 +1,94 @@
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// set up a constant for the tilt switchPin
+const int switchPin = 6;
+
+// variable to hold the value of the switchPin
+int switchState = 0;
+
+// variable to hold previous value of the switchpin
+int prevSwitchState = 0;
+
+// a variable to choose which reply from the crystal ball
+int reply;
+
+void setup() {
+  // set up the number of columns and rows on the LCD 
+  lcd.begin(16, 2);
+  
+  // set up the switch pin as an input
+  pinMode(switchPin,INPUT);
+  
+  // Print a message to the LCD.
+    lcd.print("Welcome to message center:");
+  // set the cursor to column 0, line 1
+  // line 1 is the second row, since counting begins with 0
+  lcd.setCursor(0, 1);
+  // print to the second line
+  lcd.print("Shake to change LCD");
+}
+
+void loop() {
+  // check the status of the switch
+  switchState = digitalRead(switchPin);
+
+  // compare the switchState to its previous state
+  if (switchState != prevSwitchState) {
+    // if the state has changed from HIGH to LOW
+    // you know that the ball has been tilted from 
+    // one direction to the other 
+    if (switchState == LOW) {
+      // randomly chose a reply
+      reply = random(8);
+      // clean up the screen before printing a new reply
+      lcd.clear();
+      // set the cursor to column 0, line 0     
+      lcd.setCursor(0, 0);
+      // print some text
+      lcd.print("Paul Borawski says:");
+      // move the cursor to the second line
+      lcd.setCursor(0, 1);
+
+      // choose a saying to print baed on the value in reply 
+      switch(reply){
+      case 0:
+        lcd.print("Have a great day");
+        break;
+
+      case 1:
+        lcd.print("Pizza is tasty");
+        break;
+
+      case 2:
+        lcd.print("Happy St. Patrick's Day");
+        break;
+
+      case 3:
+        lcd.print("1738");
+        break;
+
+      case 4:
+        lcd.print("Baby girl you so damn fine doe");
+        break;
+
+      case 5:
+        lcd.print("I like turtles");
+        break;
+
+      case 6:
+        lcd.print("Audi is my favorite car");
+        break;
+
+      case 7:
+        lcd.print("ANything else");
+        break;
+      }
+    }
+  }
+  // save the current switch state as the last state 
+  prevSwitchState = switchState;
+}
+
--- readme.markdown
+++ readme.markdown
@@ -3,3 +3,5 @@
 progOne changes lights when button is pressed on the circuit
 
 progTwo makes a musical circuit with four notes: C, D, E, and F
+
+progThree makes prints a random message on an LCD screen every time you shake the breadboard, you must have a tilt sensor
